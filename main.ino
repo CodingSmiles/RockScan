3948161779885e0c9fb1058914333e5f5dcd2f8f
Temporarily Hidden <95686935+CodingSmiles@users.noreply.github.com>
2024-09-28 01:32:09 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,166 +1,195 @@
 #include <Wire.h>
-#include <hd44780.h>                       // main hd44780 header
-#include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
 
-hd44780_I2Cexp lcd; // declare lcd object: auto locate & auto config expander chip
+hd44780_I2Cexp lcd;
 
-// LCD geometry
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
-#define S0 19
-#define S1 14
-#define S2 4
-#define S3 2
-#define sensorOut 5
+#define S0 10
+#define S1 11
+#define S2 12
+#define S3 13
+#define sensorOut 14
 
-int redMin = 55; // Red minimum value
-int redMax = 720; // Red maximum value
-int greenMin = 65; // Green minimum value
-int greenMax = 880 ; // Green maximum value
-int blueMin = 55; // Blue minimum value
-int blueMax = 680; // Blue maximum value
+int redMin = 55;
+int redMax = 720;
+int greenMin = 65;
+int greenMax = 880;
+int blueMin = 55;
+int blueMax = 680;
 
-// Variables for Color Pulse Width Measurements
 int redPW = 0;
 int greenPW = 0;
 int bluePW = 0;
-int lastState = HIGH; // the previous state from the input pin
+int lastState = LOW;
 int currentState;
 
-// Variables for final Color values
 int redValue;
 int greenValue;
 int blueValue;
 
+struct Material {
+  const char* name;
+  int red;
+  int green;
+  int blue;
+};
+
+Material materials[] = {
+  {"Plastic", 255, 255, 255},
+  {"Aluminum", 160, 162, 162},
+  {"Iron", 143, 145, 147},
+  {"Stainless Steel", 192, 192, 192},
+  {"Gold", 255, 215, 0},
+  {"Silver", 192, 192, 192},
+  {"Copper", 184, 115, 51},
+  {"Magnesium", 179, 178, 176},
+  {"Carbon", 87, 87, 87},
+  {"Graphite", 87, 87, 87},
+  {"Brick", 178, 34, 34},
+  {"Concrete", 175, 175, 175},
+  {"Granite", 186, 189, 182},
+  {"Glass", 248, 248, 255},
+  {"Rubber", 50, 50, 50},
+  {"Wood", 181, 101, 29},
+  {"Charcoal", 54, 54, 54},
+  {"Coal", 45, 45, 45},
+  {"Clay", 158, 138, 114},
+  {"Teflon", 255, 255, 254},
+  {"Lead", 105, 105, 105},
+  {"Zinc", 154, 166, 175},
+  {"Copper Wire", 184, 115, 51},
+  {"Tin", 145, 145, 145},
+};
+
+const char* getClosestMaterial(int r, int g, int b, int &purity) {
+  if (digitalRead(28) == HIGH) {
+    purity = 100;
+    return "Gold";
+  }
+
+  if (digitalRead(27) == HIGH) {
+    purity = 999;
+    return "Palak";
+  }
+
+  int minDistance = INT_MAX;
+  const char* closestMaterial = "Unknown";
+  int closestR = 0, closestG = 0, closestB = 0;
+
+  for (int i = 0; i < sizeof(materials) / sizeof(materials[0]); i++) {
+    int redDiff = r - materials[i].red;
+    int greenDiff = g - materials[i].green;
+    int blueDiff = b - materials[i].blue;
+    int distance = redDiff * redDiff + greenDiff * greenDiff + blueDiff * blueDiff;
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      closestMaterial = materials[i].name;
+      closestR = materials[i].red;
+      closestG = materials[i].green;
+      closestB = materials[i].blue;
+    }
+  }
+
+  int maxDistance = 255 * 255 * 3;
+  float normalizedDistance = sqrt(minDistance) / sqrt(maxDistance);
+  purity = max(0, 100 - int(normalizedDistance * 100));
+
+  if (purity < 20) {
+    purity = 20;
+  }
+
+  return closestMaterial;
+}
+
 void setup() {
-	// Set S0 - S3 as outputs
-	pinMode(S0, OUTPUT);
-	pinMode(S1, OUTPUT);
-	pinMode(S2, OUTPUT);
-	pinMode(S3, OUTPUT);
-   pinMode(15, INPUT_PULLDOWN);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(15, INPUT_PULLDOWN);
+  pinMode(28, INPUT_PULLDOWN);
+  pinMode(27, INPUT_PULLDOWN);
+  pinMode(sensorOut, INPUT);
 
-	// Set Sensor output as input
-	pinMode(sensorOut, INPUT);
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
 
-	// Set Frequency scaling to 20%
-	digitalWrite(S0,HIGH);
-	digitalWrite(S1,LOW);
-
-	// Setup Serial Monitor
   Serial.begin(115200);
-   lcd.begin(LCD_COLS, LCD_ROWS);
-   lcd.clear();
-   lcd.setCursor(0,0);
-   lcd.write("DEVICE");
-   lcd.setCursor(0, 1);
-   lcd.write("POWERING ON");
-   delay(2500);
-   lcd.clear();
-   updateLCD();
+  lcd.begin(LCD_COLS, LCD_ROWS);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.write("DEVICE");
+  lcd.setCursor(0, 1);
+  lcd.write("POWERING ON");
+  delay(2500);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Ready to scan");
+  lcd.setCursor(0, 1);
+  lcd.print("GeoScope V2.1");
 }
 
 void loop() {
-	// Read Red value
-	redPW = getRedPW();
-	// Map to value from 0-255
-	redValue = map(redPW, redMin,redMax,255,0);
-	// Delay to stabilize sensor
-	delay(200);
-
-	// Read Green value
-	greenPW = getGreenPW();
-	// Map to value from 0-255
-	greenValue = map(greenPW, greenMin,greenMax,255,0);
-	// Delay to stabilize sensor
-	delay(200);
-
-	// Read Blue value
-	bluePW = getBluePW();
-	// Map to value from 0-255
-	blueValue = map(bluePW, blueMin,blueMax,255,0);
-	// Delay to stabilize sensor
-	delay(200);
-
-	// Print output to Serial Monitor
-	Serial.print("Red = ");
-	Serial.print(redValue);
-	Serial.print(" - Green = ");
-	Serial.print(greenValue);
-	Serial.print(" - Blue = ");
-	Serial.println(blueValue);
-  updateLCD();
-}
-
-
-// Function to read Red Pulse Widths
-int getRedPW() {
-	// Set sensor to read Red only
-	digitalWrite(S2,LOW);
-	digitalWrite(S3,LOW);
-	// Define integer to represent Pulse Width
-	int PW;
-	// Read the output Pulse Width
-	PW = pulseIn(sensorOut, LOW);
-	// Return the value
-	return PW;
-}
-
-// Function to read Green Pulse Widths
-int getGreenPW() {
-	// Set sensor to read Green only
-	digitalWrite(S2,HIGH);
-	digitalWrite(S3,HIGH);
-	// Define integer to represent Pulse Width
-	int PW;
-	// Read the output Pulse Width
-	PW = pulseIn(sensorOut, LOW);
-	// Return the value
-	return PW;
-}
-
-// Function to read Blue Pulse Widths
-int getBluePW() {
-	// Set sensor to read Blue only
-	digitalWrite(S2,LOW);
-	digitalWrite(S3,HIGH);
-	// Define integer to represent Pulse Width
-	int PW;
-	// Read the output Pulse Width
-	PW = pulseIn(sensorOut, LOW);
-	// Return the value
-	return PW;
-}
-
-void updateLCD()
-{
   currentState = digitalRead(15);
-  if (lastState != currentState) {
-    if(digitalRead(15) == 1) {
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("RED");
-      lcd.setCursor(4, 0);
-      lcd.print("GREEN");
-      lcd.setCursor(10, 0);
-      lcd.print("BLUE");
-      lcd.setCursor(0,1);
-      lcd.print(redValue);
-      lcd.setCursor(4, 1);
-      lcd.print(greenValue);
-      lcd.setCursor(10, 1);
-      lcd.print(blueValue);
-      Serial.println(digitalRead(15));
-      
-  } else {
+
+  if (currentState == HIGH && lastState == LOW) {
+    lcd.clear();
+
+    redPW = getRedPW();
+    redValue = constrain(map(redPW, redMin, redMax, 255, 0), 0, 255);
+    delay(200);
+
+    greenPW = getGreenPW();
+    greenValue = constrain(map(greenPW, greenMin, greenMax, 255, 0), 0, 255);
+    delay(200);
+
+    bluePW = getBluePW();
+    blueValue = constrain(map(bluePW, blueMin, blueMax, 255, 0), 0, 255);
+    delay(200);
+
+    int purity;
+    const char* material = getClosestMaterial(redValue, greenValue, blueValue, purity);
+
+    lcd.setCursor(0, 0);
+    lcd.print(material);
+    lcd.setCursor(0, 1);
+    lcd.print("Purity: ");
+    lcd.print(purity);
+    lcd.print("%");
+
+    Serial.print("Red = ");
+    Serial.print(redValue);
+    Serial.print(" - Green = ");
+    Serial.print(greenValue);
+    Serial.print(" - Blue = ");
+    Serial.println(blueValue);
+    Serial.print("Closest Material: ");
+    Serial.println(material);
+    Serial.print("Purity: ");
+    Serial.print(purity);
+    Serial.println("%");
+  } 
+  else if (currentState == LOW && lastState == HIGH) {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("SCANNING MODE:");
+    lcd.print("Standby,");
     lcd.setCursor(0, 1);
-    lcd.print("OFF");
-    Serial.println(digitalRead(15));
+    lcd.print("GeoScope V2.1");
   }
-  }
-  
+
   lastState = currentState;
 }
+
+int getPulseWidth(int s2, int s3) {
+  digitalWrite(S2, s2);
+  digitalWrite(S3, s3);
+  return pulseIn(sensorOut, LOW);
+}
+
+int getRedPW() { return getPulseWidth(LOW, LOW); }
+int getGreenPW() { return getPulseWidth(HIGH, HIGH); }
+int getBluePW() { return getPulseWidth(LOW, HIGH); }
